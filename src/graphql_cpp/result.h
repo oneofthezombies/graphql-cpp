82c39ad02cfeb403a567edb55f0ca82fb2901596
oneofthezombies <hunhoekim@gmail.com>
2025-01-28 10:32:30 +0000

    ♻️ Remove duplicated implementations

--- src/graphql_cpp/result.h
+++ src/graphql_cpp/result.h
@@ -117,74 +117,80 @@ template <typename T, typename E, typename F>
   return detail::MapErr<T, E2>(std::move(r), std::forward<F>(f));
 }
 
+namespace detail {
+
+template <typename T, typename E, typename R2, typename R, typename F>
+[[nodiscard]] constexpr auto AndThen(R&& r, F&& f) {
+  if (IsOk(r)) {
+    return f(Unwrap(std::forward<R>(r)));
+  } else {
+    return R2{std::in_place_index<1>, UnwrapErr(std::forward<R>(r))};
+  }
+}
+
+template <typename T, typename E, typename R2, typename R, typename F>
+[[nodiscard]] constexpr auto OrElse(R&& r, F&& f) {
+  if (IsErr(r)) {
+    return f(UnwrapErr(std::forward<R>(r)));
+  } else {
+    return R2{std::in_place_index<0>, Unwrap(std::forward<R>(r))};
+  }
+}
+
+}  // namespace detail
+
 template <typename T, typename E, typename F>
   requires std::invocable<F, const T&>
 [[nodiscard]] constexpr auto AndThen(const Result<T, E>& r, F f) noexcept {
-  using R = std::invoke_result_t<F, const T&>;
+  using R2 = std::invoke_result_t<F, const T&>;
 
-  static_assert(std::variant_size_v<R> == 2,
+  static_assert(std::variant_size_v<R2> == 2,
                 "Return type must be a Result (variant with two types)");
   static_assert(
-      std::is_same_v<std::variant_alternative_t<1, R>, E>,
+      std::is_same_v<std::variant_alternative_t<1, R2>, E>,
       "Second type of return type must be the same as the error type");
 
-  if (IsOk(r)) {
-    return f(Unwrap(r));
-  } else {
-    return R{std::in_place_index<1>, UnwrapErr(r)};
-  }
+  return detail::AndThen<T, E, R2>(r, std::forward<F>(f));
 }
 
 template <typename T, typename E, typename F>
   requires std::invocable<F, T&&>
 [[nodiscard]] constexpr auto AndThen(Result<T, E>&& r, F f) noexcept {
-  using R = std::invoke_result_t<F, T&&>;
+  using R2 = std::invoke_result_t<F, T&&>;
 
-  static_assert(std::variant_size_v<R> == 2,
+  static_assert(std::variant_size_v<R2> == 2,
                 "Return type must be a Result (variant with two types)");
   static_assert(
-      std::is_same_v<std::variant_alternative_t<1, R>, E>,
+      std::is_same_v<std::variant_alternative_t<1, R2>, E>,
       "Second type of return type must be the same as the error type");
 
-  if (IsOk(r)) {
-    return f(Unwrap(std::move(r)));
-  } else {
-    return R{std::in_place_index<1>, UnwrapErr(std::move(r))};
-  }
+  return detail::AndThen<T, E, R2>(std::move(r), std::forward<F>(f));
 }
 
 template <typename T, typename E, typename F>
   requires std::invocable<F, const E&>
 [[nodiscard]] constexpr auto OrElse(const Result<T, E>& r, F f) noexcept {
-  using R = std::invoke_result_t<F, const E&>;
+  using R2 = std::invoke_result_t<F, const E&>;
 
-  static_assert(std::variant_size_v<R> == 2,
+  static_assert(std::variant_size_v<R2> == 2,
                 "Return type must be a Result (variant with two types)");
-  static_assert(std::is_same_v<std::variant_alternative_t<0, R>, T>,
+  static_assert(std::is_same_v<std::variant_alternative_t<0, R2>, T>,
                 "First type of return type must be the same as the value type");
 
-  if (IsErr(r)) {
-    return f(UnwrapErr(r));
-  } else {
-    return R{std::in_place_index<0>, Unwrap(r)};
-  }
+  return detail::OrElse<T, E, R2>(r, std::forward<F>(f));
 }
 
 template <typename T, typename E, typename F>
   requires std::invocable<F, E&&>
 [[nodiscard]] constexpr auto OrElse(Result<T, E>&& r, F f) noexcept {
-  using R = std::invoke_result_t<F, E&&>;
+  using R2 = std::invoke_result_t<F, E&&>;
 
-  static_assert(std::variant_size_v<R> == 2,
+  static_assert(std::variant_size_v<R2> == 2,
                 "Return type must be a Result (variant with two types)");
-  static_assert(std::is_same_v<std::variant_alternative_t<0, R>, T>,
+  static_assert(std::is_same_v<std::variant_alternative_t<0, R2>, T>,
                 "First type of return type must be the same as the value type");
 
-  if (IsErr(r)) {
-    return f(UnwrapErr(std::move(r)));
-  } else {
-    return R{std::in_place_index<0>, Unwrap(std::move(r))};
-  }
+  return detail::OrElse<T, E, R2>(std::move(r), std::forward<F>(f));
 }
 
 }  // namespace graphql_cpp
